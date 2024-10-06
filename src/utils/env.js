fab77d625d6db2bcc50204080c9c49ac0791ab6a
Eduard <edik.moskalets@gmail.com>
2024-10-06 21:39:31 +0300

    upd

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,10 +1,11 @@
 // # const DB_URI = "mongodb+srv://edikmoskalets:<db_password>@cluster0.5qu9w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+// "mongodb+srv://edikmoskalets:<db_password>@contacts.zx9j6.mongodb.net/?retryWrites=true&w=majority&appName=Contacts"
 
 // PORT=3000
 // MONGODB_USER=edikmoskalets
 // MONGODB_PASSWORD=hZjbTAGrjIdSm7PZ
-// MONGODB_URL=cluster0.5qu9w.mongodb.net
-// MONGODB_DB=students
+// MONGODB_URL=contacts.zx9j6.mongodb.net
+// MONGODB_DB=contacts
 
 import dotenv from 'dotenv';
 dotenv.config();
