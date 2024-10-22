0c6b399611088e4ab52e296b840c4b21996aa55c
Eduard <edik.moskalets@gmail.com>
2024-10-22 11:34:36 +0300

    upd

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -1,10 +1,24 @@
+// import { isValidObjectId } from 'mongoose';
+// import { HttpError } from 'http-errors';
+
+// export const isValidId = (req, res, next) => {
+//   const { id } = req.params;
+//   if (!isValidObjectId(id)) {
+//     throw HttpError(404, 'Not found');
+//   }
+
+//   next();
+// };
+
+// =========================================================================
+
 import { isValidObjectId } from 'mongoose';
-import { HttpError } from 'http-errors';
+import createHttpError, { HttpError } from 'http-errors';
 
 export const isValidId = (req, res, next) => {
-  const { id } = req.params;
-  if (!isValidObjectId(id)) {
-    throw HttpError(404, 'Not found');
+  const {contactId}  = req.params;
+  if (!isValidObjectId(contactId)) {
+    throw createHttpError(404, 'Not found');
   }
 
   next();
